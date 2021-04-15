e94e3788a0856c5220a70233deba13e2927a0fd2
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2021-04-15 17:32:21 +0000

    chore: add gitignore

--- ipfs-add.sh
+++ ipfs-add.sh
@@ -3,4 +3,4 @@
 curl \
     -X POST \
     -H 'Blockchain-Subdomain: ipfs' \
-    -F file=@myfile "http://localhost:3000/v1/$applicationId/api/v0/add?quiet=true&quieter=true&progress=false&chunker=size-262144&pin=true&hash=sha2-256&inline-limit=32"
+    -F file=@myfile "http://localhost:3000/v1/60676c9f7cbbfe002f0b9cbe/api/v0/add?quiet=true&quieter=true&progress=false&chunker=size-262144&pin=true&hash=sha2-256&inline-limit=32"
