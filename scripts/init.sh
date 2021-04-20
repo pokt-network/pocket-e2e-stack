d5845d4bff10d4693d58dbd099f64c3c9ce9cc87
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2021-04-20 18:50:48 +0000

    chore: add everything

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,18 +3,25 @@
 set ppkfilepath $argv; # Grab the first command line parameter
 set timeout -1
 
-spawn sh -c cp /tmp/*.json /home/app/.pocket/config/
-spawn sh -c echo $ppkfilepath
-spawn pocket accounts import-armored $ppkfilepath
+log_user 1;
+
+spawn sh -c "mkdir -p /home/app/.pocket/config"
+spawn sh -c "cp /tmp/*.json /home/app/.pocket/config/"
+spawn sh -c "echo $ppkfilepath"
+spawn sh -c "pocket accounts import-armored $ppkfilepath"
+
 sleep 2
+
 send -- "$env(POCKET_CORE_PASSPHRASE)\n"
 send -- "$env(POCKET_CORE_PASSPHRASE)\n"
+
 expect eof
+
 spawn sh -c "echo \"done with import armored\"";
 
 spawn sh -c "pocket accounts set-validator `pocket accounts list | cut -d' ' -f2- `"
 sleep 5
 send -- "$env(POCKET_CORE_PASSPHRASE)\n"
 expect eof
+
 spawn sh -c "echo \"done with set-validator\""
-spawn sh -c pocket start --keybase=false --mainnet --datadir=/home/app/.pocket/
---
+++ scripts/ipfs-ls.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl \
+    -H 'Content-Type: application/json' \
+    -H 'Blockchain-Subdomain: ipfs' \
+    -H 'Origin: Moonbase' \
+    -X POST http://localhost:3000/v1/607d9f3064c40bc97ff5cb81/api/v0/ls?arg=/ipfs/Qmexhq2sBHnXQbvyP2GfUdbnY7HCagH2Mw5vUNSBn2nxip \
+    -d '{}'
