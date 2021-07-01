1fa8f4d3e919158c33415a850aad0b8aba7a3f43
derrandz <ouaghad.hamza@gmail.com>
2021-07-01 21:11:44 +0100

    chore: added a script to play the playbook, also removed force-recreate to preserve swarm state

---
+++ scripts/firefight-playbook/dumbscript.sh
@@ -0,0 +1,2 @@
+echo "Hi from dumbscript"
+echo $POCKET_CORE_TARGET_VERSION $SPECIFIC_NODE
--- stacks/pokt-net/prod/up.sh
+++ stacks/pokt-net/prod/up.sh
@@ -6,9 +6,21 @@ then
   exit 1;
 fi
 
-docker-compose \
-  -f $CWD/stacks/pokt-net/prod/stack.yml \
-  --project-directory $CWD/ \
-  up \
-  --build \
-  --force-recreate
+if [ -z $SPECIFIC_NODE ];
+then
+  docker-compose \
+    -f $CWD/stacks/pokt-net/prod/stack.yml \
+    --project-directory $CWD/ \
+    up \
+    --build
+#    --build \
+#    --force-recreate
+else
+  docker-compose \
+    -f $CWD/stacks/pokt-net/prod/stack.yml \
+    --project-directory $CWD/ \
+    up \
+    --build \
+    $SPECIFIC_NODE
+#    --force-recreate \
+fi
