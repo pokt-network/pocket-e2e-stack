38d93023be86dbb94fcdafda61e4f0dbc9552a9c
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-03-25 20:38:46 -0700

    Trying to improve prepare tendermint

--- docker/pokt-net/dev-het/prepare-tendermint.sh
+++ docker/pokt-net/dev-het/prepare-tendermint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# if [[ -f "$POCKET_ROOT/go.mod.tendermint" ]]; then
-#   mv $POCKET_ROOT/go.mod.tendermint $TENDERMINT_PATH/go.mod
-#   cd $TENDERMINT_PATH
-#   echo "Installing dependencies for tendermint..."
-#   go mod download
-# fi
+if [[ -f "$POCKET_ROOT/go.mod.tendermint" ]]; then
+  cp $POCKET_ROOT/go.mod.tendermint $TENDERMINT_PATH/go.mod
+  cd $TENDERMINT_PATH
+  echo "Installing dependencies for tendermint..."
+  go mod download
+fi
 
 if [[ -f "$POCKET_ROOT/go.mod.core" ]]; then
   cp $POCKET_ROOT/go.mod.core $POCKET_PATH/go.mod
--- docker/pokt-net/dev-tm/prepare-tendermint.sh
+++ docker/pokt-net/dev-tm/prepare-tendermint.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-# if [[ -f "$POCKET_ROOT/go.mod.tendermint" ]]; then
-#   mv $POCKET_ROOT/go.mod.tendermint $TENDERMINT_PATH/go.mod
-#   cd $TENDERMINT_PATH
-#   echo "Installing dependencies for tendermint..."
-#   go mod download
-# fi
+if [[ -f "$POCKET_ROOT/go.mod.tendermint" ]]; then
+  # Doing a `cp` here errors; concurrency issue
+  mv $POCKET_ROOT/go.mod.tendermint $TENDERMINT_PATH/go.mod
+  cd $TENDERMINT_PATH
+  echo "Installing dependencies for tendermint..."
+  # go mod vendor
+  go mod download
+fi
+
+cd $TENDERMINT_PATH
+go build cmd/tendermint/main.go
 
 if [[ -f "$POCKET_ROOT/go.mod.core" ]]; then
+  # Doing a `mv` here errors;  can't preserve ownershi
   cp $POCKET_ROOT/go.mod.core $POCKET_PATH/go.mod
   cd $POCKET_PATH
   echo "Installing dependencies for core..."
+  # go mod vendor
   go mod download
 fi
 
